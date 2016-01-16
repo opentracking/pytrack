313422ee72a79959572350ec142c912b00b3ebf0
Ben Brook <brookb@oregonstate.edu>
2016-01-16 00:14:55 +0000

    picamera support

--- src/face_detect.py
+++ src/face_detect.py
@@ -1,5 +1,9 @@
 import cv2
 import sys
+from picamera import PiCamera
+from picamera.array import PiRGBArray
+import time
+
 
 # Get user supplied values
 cascPath = sys.argv[1]
@@ -7,16 +11,23 @@ cascPath = sys.argv[1]
 # Create the haar cascade
 faceCascade = cv2.CascadeClassifier(cascPath)
 
-video_capture = cv2.VideoCapture(0)
-
 old_x = 0
 old_y = 0
-while True:
-	ret, frame = video_capture.read()	
-	# Resize and flip frame for better performance
-	small = cv2.resize(cv2.flip(frame, 1), (640, 360))
 
-	gray = cv2.cvtColor(small, cv2.COLOR_BGR2GRAY)
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+
+time.sleep(0.1)
+
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	# Resize and flip frame for better performance
+	
+	#small_frame = frame.array
+	small_frame = cv2.resize(cv2.flip(frame.array, 1), (320, 240))
+
+	gray = cv2.cvtColor(small_frame, cv2.COLOR_BGR2GRAY)
 
 	faces = faceCascade.detectMultiScale(
 		gray,
@@ -32,14 +43,13 @@ while True:
 		# print "x: {}, y: {}, w: {}, h: {}".format(x, y, w, h)
 		old_x = x
 		old_y = y
-		cv2.rectangle(small, (x, y), (x+w, y+h), (0,255,0), 2)
+		cv2.rectangle(small_frame, (x, y), (x+w, y+h), (0,255,0), 2)
 
 	# Display the resulting frame
-	cv2.imshow('Video', small)
+	cv2.imshow('Video', small_frame)
+
+	rawCapture.truncate(0)
 
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
 
-# When everything is done, release the capture
-video_capture.release()
-cv2.destroyAllWindows()
