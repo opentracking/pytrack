a7a5a13ccc95171ceffa1585ccc7b060bb16ed16
Ben Brook <brookb@oregonstate.edu>
2016-03-09 23:54:17 +0000

    Add library install to script

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 apt-get update
 apt-get install python-opencv -y
+apt-get install libgli-mesa-*
+ldconfig
 git clone https://github.com/opentracking/pytrack.git pytrack_example
 cd pytrack_example
 python test.py cascade/haarcascade_frontalface_default.xml
+
