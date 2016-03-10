a73a3fd2af1fdf6eeb65b094aab2b53add554d21
Justin "Chetco" Brown <brownju@oregonstate.edu>
2016-03-09 16:09:31 -0800

    add rm cmd line arg
    Signed-off-by: Justin "Chetco" Brown <brownju@oregonstate.edu>

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-apt-get update
-apt-get install python-opencv -y
-apt-get install libgl1-mesa-*
-ldconfig
-git clone https://github.com/opentracking/pytrack.git pytrack_example
-cd pytrack_example
-python test.py cascade/haarcascade_frontalface_default.xml
+if [[ $1 == "rm" ]]; then
+    apt-get update
+    apt-get remove python-opencv -y
+    ldconfig
+else
+    apt-get update
+    apt-get install python-opencv -y
+    apt-get install libgl1-mesa-*
+    ldconfig
+    git clone https://github.com/opentracking/pytrack.git pytrack_example
+    cd pytrack_example
+    python test.py cascade/haarcascade_frontalface_default.xml
+fi
 
