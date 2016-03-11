fd009df52533244e90143b71481169e675799a90
ErnestoGP <ErnestoGPerez91@yahoo.com>
2016-03-10 23:49:42 -0800

    updated MANIFEST

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 setup(
 	name = 'Pytrack',
 	packages = ['Pytrack'],
-	version = '0.0.3',
+	version = '0.0.4',
 	description = 'face tracking software for use with the raspberry pi.',
 	author = 'Ben Brook, Justin Brown, Ernesto Perez',
 	url = 'https://github.com/opentracking/pytrack',
-	download_url = '://github.com/opentracking/pytrack/archive/0.0.3.tar.gz',
+	download_url = '://github.com/opentracking/pytrack/archive/0.0.4.tar.gz',
 	keywords = ['track', 'pi', 'face', 'pytrack', 'motor'],
 	classifiers = [],
 	)
