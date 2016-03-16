4ffc2744167381954b8f85ad39f3c68e33e427f5
ErnestoGP <ErnestoGPerez91@yahoo.com>
2016-03-15 23:34:10 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
 	description = 'face tracking software for use with the raspberry pi.',
 	author = 'Ben Brook, Justin Brown, Ernesto Perez',
 	url = 'https://github.com/opentracking/pytrack',
-	download_url = '://github.com/opentracking/pytrack/archive/0.0.5.tar.gz',
+	download_url = '://github.com/opentracking/pytrack/archive/0.0.6.tar.gz',
 	keywords = ['track', 'pi', 'face', 'pytrack', 'motor'],
 	classifiers = [],
 	)
