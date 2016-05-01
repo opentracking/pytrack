9f6b9d3e04f8bb81312696c65def788d33662f4b
Ben Brook <bbrook154@gmail.com>
2016-05-01 10:45:02 -0700

    Update tabs

--- motor.py
+++ motor.py
@@ -3,60 +3,57 @@ import time
 import os
 
 class Motor(object):
-    def __init__(self):
-        """
-        pixels_x : number of horizontal pixels of image capture
-        pixels_y : number of vertical pixels of image capture
-        fov_x : degrees of horizontal camera perspective
-        fov_y : degrees of vertical camera perspective
-        """
+	def __init__(self):
+		"""
+		TO ADD
+		"""
 
-        self.delay = 0.06
-        self.current_x = 135
-        self.current_y = 90
+		self.delay = 0.06
+		self.current_x = 135
+		self.current_y = 90
 
-        self.x_pin = 0
-        self.y_pin = 1
+		self.x_pin = 0
+		self.y_pin = 1
 
-        # Manually set the position to start
-        self.pwm(self.x_pin, self.current_x, self.delay)
-        self.pwm(self.y_pin, self.current_y, self.delay)
+		# Manually set the position to start
+		self.pwm(self.x_pin, self.current_x, self.delay)
+		self.pwm(self.y_pin, self.current_y, self.delay)
 
-    def update_x(self, dx):
-        """
-        dx : horizontal pixels from image center
+	def update_x(self, dx):
+		"""
+		dx : horizontal pixels from image center
 
-        Update the servo horizontal position
-        """
+		Update the servo horizontal position
+		"""
 
-        movement_x = self.current_x + dx
-        
-        self.pwm(self.x_pin, movement_x, self.delay)
-        
-        self.current_x = movement_x
+		movement_x = self.current_x + dx
+		
+		self.pwm(self.x_pin, movement_x, self.delay)
+		
+		self.current_x = movement_x
 
-    def update_y(self, dy):
-        """
-        dy : vertical pixels from image center
+	def update_y(self, dy):
+		"""
+		dy : vertical pixels from image center
 
-        Update the servo vertical position
-        """
+		Update the servo vertical position
+		"""
 
-        movement_y = self.current_y + dy
+		movement_y = self.current_y + dy
 
-        self.pwm(self.y_pin, movement_y, self.delay)
+		self.pwm(self.y_pin, movement_y, self.delay)
 
-        self.current_y = movement_y
+		self.current_y = movement_y
 
-    def pwm(self,pin,offset,delay):
-        """
-        pin    : GPIO pin of servo
-        offset : Amount servo should move
-        delay  : Amount of time to wait for the servo to move
+	def pwm(self,pin,offset,delay):
+		"""
+		pin    : GPIO pin of servo
+		offset : Amount servo should move
+		delay  : Amount of time to wait for the servo to move
 
-        """
-        print("servo[" + str(pin) + "][" + str(offset) + "]")
-        cmd = "echo " + str(pin) + "=" + str(offset) + " > /dev/servoblaster"
-        os.system(cmd)
-        time.sleep(delay)
+		"""
+		print("servo[" + str(pin) + "][" + str(offset) + "]")
+		cmd = "echo " + str(pin) + "=" + str(offset) + " > /dev/servoblaster"
+		os.system(cmd)
+		time.sleep(delay)
 	
