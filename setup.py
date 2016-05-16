6a3990bf853093868377be4f0b39a0afef1f46db
Ernesto Perez <perezer@flip1.engr.oregonstate.edu>
2016-05-16 16:12:15 -0700

    there was mistakes in setup.py. I fixed them.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 setup(
 	name = 'Pytrack',
 	packages = ['Pytrack'],
-	version = '1.0',
+	version = '1.0.1',
 	description = 'face tracking software for use with the raspberry pi.',
 	author = 'Ben Brook, Justin Brown, Ernesto Perez',
 	url = 'https://github.com/opentracking/pytrack',
-	download_url = '://github.com/opentracking/pytrack/archive/1.0.tar.gz',
+	download_url = 'https://github.com/opentracking/pytrack/archive/1.0.1.tar.gz',
 	keywords = ['track', 'pi', 'face', 'pytrack', 'motor'],
 	classifiers = [],
 	)
